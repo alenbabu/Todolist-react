de0da08aa2dbdb133f4f160a9185e27b9f3f8c9b
Alen Babu <alenbabualen@gmail.com>
2023-10-27 11:31:19 +0530

    design and script modified

--- src/App.js
+++ src/App.js
@@ -2,40 +2,46 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [todos,setTodos] = useState([])
-  const [text,setText] = useState('')
+  const [todos, setTodos] = useState([])
+  const [text, setText] = useState('')
 
   return (
-    <div className="app">
-      <div className="mainHeading">
-        <h1>ToDo List</h1>
-      </div>
-      <div className="subHeading">
-        <br />
-        <h2>Whoops, it's Wednesday ☕ </h2>
-      </div>
-      <div className="input">
-        <input value={text} onChange={(e)=>setText(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=> setTodos([...todos,text])} className="fas fa-plus"></i>
-      </div>
-      {todos.map((value)=>{
-        return(
-          <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>{value}</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
+    <div className="main-body">
+      <div className="app">
+        <div className="heading">
+          <h1>Todo List </h1>
+          <h2>Note your todo Now!!</h2>
+
+        </div>
+        <div className="input">
+          <input type="text" placeholder="🖊️ Add item..." />
+          <i className="fas fa-plus"></i>
         </div>
       </div>
-        )
+      <div className="task">
+        <div className="active-task">
+          <h1>ACTIVE TASK</h1>
+          <div className="todo">
+            <div className="left">
+              <input type="checkbox" name="" id="" />
+              <p>heloooooooo</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+        </div>
+        <div className="done-task">
+          <h1>DONE TASK</h1>
 
-      })}
-      
-    </div>
+        </div>
+        <div className="deleted-task">
+          <h1>DELETED TASK</h1>
+
+        </div>
+      </div>
+    </div >
+
   );
 }
 
